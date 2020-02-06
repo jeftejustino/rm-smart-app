f4a30bca3800432983711699723db3ec68b955ad
Jefte Justino <jefte.justino@gmail.com>
2020-02-06 15:13:15 -0300

    Create component Select

---
+++ src/components/Select/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Container, Picker} from './styles';
+
+export default function Select({options, ...rest}) {
+  return (
+    <Container>
+      <Picker {...rest}>
+        {options.map(item => (
+          <Picker.Item value={item.value} key={item.value} label={item.label} />
+        ))}
+      </Picker>
+    </Container>
+  );
+}
+
+Select.propTypes = {
+  options: PropTypes.array.isRequired,
+};
---
+++ src/components/Select/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background: #fff;
+  margin: 8px 0px;
+  padding: 0px 10px;
+  height: 40px;
+`;
+
+export const Picker = styled.Picker`
+  height: 40px;
+`;
