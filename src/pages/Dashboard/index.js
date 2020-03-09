ccbc6e9a8e06967687749f6b1c9120927c05ea7b
Jefte Justino <jefte.justino@gmail.com>
2020-03-09 11:47:01 -0300

    Feature Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import api from '~/services/api';
 
 import {Container, Chart} from './styles';
 
@@ -7,18 +9,65 @@ import People from './People';
 import Business from './Business';
 
 export default function Dashboard() {
+  const [views, setViews] = useState({
+    '2000-01-01': 1,
+    '2000-01-02': 1,
+    '2000-01-03': 1,
+    '2000-01-04': 1,
+    '2000-01-05': 1,
+    '2000-01-06': 1,
+    '2000-01-07': 1,
+  });
+
+  const [people, setPeople] = useState({
+    '2000-01-01': 1,
+    '2000-01-02': 1,
+    '2000-01-03': 1,
+    '2000-01-04': 1,
+    '2000-01-05': 1,
+    '2000-01-06': 1,
+    '2000-01-07': 1,
+  });
+
+  const [business, setBusiness] = useState({
+    '2000-01-01': 1,
+    '2000-01-02': 1,
+    '2000-01-03': 1,
+    '2000-01-04': 1,
+    '2000-01-05': 1,
+    '2000-01-06': 1,
+    '2000-01-07': 1,
+  });
+
+  async function getData() {
+    try {
+      const response = await api.get('dashboard');
+      const {visitas, pessoas, vendas} = response.data;
+
+      setViews(visitas);
+      setPeople(pessoas);
+      setBusiness(vendas);
+    } catch (error) {
+      // error
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <Container>
       <Chart>
-        <Views />
+        <Views info={views} />
       </Chart>
 
       <Chart>
-        <People />
+        <People info={people} />
       </Chart>
 
       <Chart>
-        <Business />
+        <Business info={business} />
       </Chart>
     </Container>
   );
