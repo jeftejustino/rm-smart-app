447cf17259f0df7235db9c635264c5dac373b883
Jefte Justino <jefte.justino@gmail.com>
2020-03-18 14:36:08 -0300

    Feature business list

--- src/pages/Business/List/index.js
+++ src/pages/Business/List/index.js
@@ -1,8 +1,235 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Alert, ActivityIndicator, FlatList} from 'react-native';
+import PropTypes from 'prop-types';
+import api from '~/services/api';
+import {formatPrice} from '~/util/format';
 
-import {Container} from './styles';
+import Select from '~/components/Select';
+import Button from '~/components/Button';
 
-export default function List() {
-  return <Container />;
+import {
+  Container,
+  Header,
+  Pipeline,
+  PPItem,
+  PPItemHeader,
+  PPItemName,
+  PPItemInfo,
+  PPItemPrice,
+  PPItemPriceSuffix,
+  PPItemQtd,
+  PPItemContent,
+  PPB,
+  PPBName,
+  PPBClient,
+  PPBDays,
+  PPBPrice,
+  PPBPriceSuffix,
+  PPBTop,
+  PPBBottom,
+  HeaderInfo,
+  HeaderPrice,
+  HeaderQtd,
+} from './styles';
+
+export default function BusinessList({navigation}) {
+  const [totalQtd, setTotalQtd] = useState(0);
+  const [totalPrice, setTotalPrice] = useState('0,00');
+  const [loading, setLoading] = useState(true);
+  const [etapas, setEtapas] = useState([]);
+  const [pipelines, setPipelines] = useState([]);
+  const [pipeline, setPipeline] = useState(0);
+  const [options, setOptions] = useState([]);
+  const [status, setStatus] = useState(0);
+  const [firstOrder, setfirstOrder] = useState(0);
+  const optionsStatus = [
+    {value: 0, label: 'Em Andamento'},
+    {value: -1, label: 'Ganho(s)'},
+    {value: 1, label: 'Perdido(s)'},
+  ];
+  const optionsFirstOrder = [
+    {value: 0, label: 'Selecione'},
+    {value: 1, label: 'Primeira Compra'},
+    {value: 2, label: 'Recompra (Cliente)'},
+  ];
+
+  async function loadPipelines() {
+    try {
+      const response = await api.get('negocio/funis');
+
+      setPipelines(response.data);
+      setEtapas(response.data[0].etapas);
+      setPipeline(response.data[0].id);
+    } catch (error) {
+      Alert.alert('Erro ao carregar Pipelines');
+    }
+  }
+
+  async function loadPipeline() {
+    setLoading(true);
+    try {
+      const response = await api.get(`negocio/${pipeline}`, {
+        params: {
+          status,
+          cliente: firstOrder,
+        },
+      });
+
+      let pipelineEtapas = [];
+      pipelines.forEach(item => {
+        if (item.id === pipeline) {
+          pipelineEtapas = item.etapas;
+        }
+      });
+
+      let q2 = 0;
+      let price2 = 0;
+      const etps = pipelineEtapas.map(item => {
+        let q = 0;
+        let price = 0;
+        const business = [];
+
+        if (response.data[item.id]) {
+          response.data[item.id].forEach(element => {
+            price += parseInt(element.valor, 10);
+            price2 += parseInt(element.valor, 10);
+            q += 1;
+            q2 += 1;
+            business.push({
+              ...element,
+              dias: 0,
+              cliente: element.cliente_nome || element.cliente_empresa,
+              price_formatted: formatPrice(
+                parseInt(element.valor, 10) / 100,
+              ).replace('R$', ''),
+            });
+          });
+        }
+
+        return {
+          ...item,
+          business,
+          qtd: q,
+          price_formatted: formatPrice(price / 100).replace('R$', ''),
+        };
+      });
+
+      setTotalPrice(formatPrice(price2 / 100).replace('R$', ''));
+      setTotalQtd(q2);
+
+      setEtapas(etps);
+    } catch (error) {
+      Alert.alert('Erro ao carregar Pipelines');
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    loadPipelines();
+  }, []);
+
+  useEffect(() => {
+    if (pipelines) {
+      const opts = [];
+      pipelines.forEach(item => {
+        opts.push({
+          value: item.id,
+          label: item.nome,
+        });
+      });
+      setOptions(opts);
+    }
+  }, [pipelines]);
+
+  useEffect(() => {
+    if (pipeline) loadPipeline();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pipeline, status, firstOrder]);
+
+  return (
+    <Container>
+      <Header>
+        <HeaderInfo>
+          <HeaderPrice>R$ {totalPrice}</HeaderPrice>
+          <HeaderQtd>{totalQtd} Negócios</HeaderQtd>
+        </HeaderInfo>
+
+        <Select
+          options={options}
+          selectedValue={pipeline}
+          onValueChange={value => setPipeline(value)}
+        />
+        <Select
+          options={optionsStatus}
+          selectedValue={status}
+          onValueChange={value => setStatus(value)}
+        />
+        <Select
+          options={optionsFirstOrder}
+          selectedValue={firstOrder}
+          onValueChange={value => setfirstOrder(value)}
+        />
+
+        <Button onPress={() => navigation.navigate('BusinessNew')}>
+          Adicionar Negócio
+        </Button>
+      </Header>
+
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Pipeline
+          horizontal
+          data={etapas}
+          keyExtractor={item => item.id}
+          numColumns={1}
+          renderItem={({item}) => (
+            <PPItem>
+              <PPItemHeader color={item.cor}>
+                <PPItemName color={item.cor2}>{item.nome}</PPItemName>
+                <PPItemInfo>
+                  <PPItemPrice color={item.cor2}>
+                    <PPItemPriceSuffix>R$</PPItemPriceSuffix>{' '}
+                    {item.price_formatted}
+                  </PPItemPrice>
+                  <PPItemQtd color={item.cor2}>Qtd: {item.qtd}</PPItemQtd>
+                </PPItemInfo>
+              </PPItemHeader>
+              <PPItemContent>
+                {item.business && (
+                  <FlatList
+                    data={item.business}
+                    keyExtractor={business => business.id}
+                    numColumns={1}
+                    renderItem={({item: business}) => (
+                      <PPB color={item.cor}>
+                        <PPBTop>
+                          <PPBName>{business.nome}</PPBName>
+                          <PPBDays>{business.dias_info}</PPBDays>
+                        </PPBTop>
+
+                        <PPBBottom>
+                          <PPBPrice color={business.cor2}>
+                            <PPBPriceSuffix>R$</PPBPriceSuffix>{' '}
+                            {business.price_formatted}
+                          </PPBPrice>
+
+                          <PPBClient>{business.cliente}</PPBClient>
+                        </PPBBottom>
+                      </PPB>
+                    )}
+                  />
+                )}
+              </PPItemContent>
+            </PPItem>
+          )}
+        />
+      )}
+    </Container>
+  );
 }
+
+BusinessList.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
--- src/pages/Business/List/styles.js
+++ src/pages/Business/List/styles.js
@@ -1,3 +1,111 @@
 import styled from 'styled-components/native';
+import {Dimensions} from 'react-native';
 
-export const Container = styled.View``;
+export const Container = styled.ScrollView``;
+
+export const Header = styled.View`
+  padding: 0px 10px 15px;
+`;
+
+export const Total = styled.Text``;
+
+export const Qtd = styled.Text``;
+
+export const Pipeline = styled.FlatList`
+  flex-grow: 1;
+`;
+
+export const PPItem = styled.View`
+  width: ${() => Dimensions.get('window').width}px;
+`;
+
+export const PPItemHeader = styled.View`
+  background: ${props => (props.color ? props.color : '#fff')};
+  padding: 6px 0px;
+`;
+
+export const PPItemName = styled.Text`
+  color: ${props => (props.color ? props.color : '#000')};
+  text-align: center;
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+export const PPItemInfo = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  padding-top: 5px;
+`;
+
+export const PPItemPrice = styled.Text`
+  color: ${props => (props.color ? props.color : '#000')};
+  padding-right: 10px;
+  border-right-color: ${props => (props.color ? props.color : '#000')};
+  border-right-width: 1px;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const PPItemPriceSuffix = styled.Text`
+  font-weight: normal;
+  font-size: 13px;
+`;
+
+export const PPItemQtd = styled.Text`
+  color: ${props => (props.color ? props.color : '#000')};
+  padding-left: 10px;
+`;
+
+export const PPItemContent = styled.View``;
+
+export const PPB = styled.View`
+  background: #fff;
+  border-bottom-width: 1px;
+  border-bottom-color: ${props => (props.color ? props.color : '#000')};
+  padding: 5px 10px;
+`;
+
+export const PPBName = styled.Text`
+  font-weight: bold;
+  color: #000;
+`;
+
+export const PPBClient = styled.Text``;
+
+export const PPBDays = styled.Text``;
+
+export const PPBPrice = styled.Text`
+  border-right-width: 1px;
+  border-right-color: #000;
+  margin-right: 10px;
+  padding-right: 10px;
+`;
+
+export const PPBPriceSuffix = styled.Text``;
+
+export const PPBTop = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const PPBBottom = styled.View`
+  flex-direction: row;
+`;
+
+export const HeaderInfo = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  margin: 10px 0px 10px;
+`;
+
+export const HeaderQtd = styled.Text`
+  color: #000;
+  font-size: 12px;
+  padding-top: 4px;
+`;
+
+export const HeaderPrice = styled.Text`
+  font-size: 18px;
+  color: #000;
+  font-weight: bold;
+`;
